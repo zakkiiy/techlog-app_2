75bd6b662025a07b8e239674d8fda0019333d7ac
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-09-16 01:56:17 +0900

    configの修正

--- config/application.rb
+++ config/application.rb
@@ -12,13 +12,13 @@ module TechlogApp
     config.load_defaults 7.0
 
     config.generators do |g| # ここから追記
-      g.assets false          # CSS, JavaScriptファイルを自動生成しない
-      g.helper false      # helperファイルを自動生成しない
+      g.assets false # CSS, JavaScriptファイルを自動生成しない
+      g.helper false # helperファイルを自動生成しない
       g.test_framework :rspec, # ここから5行を追記
-        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
-        view_specs: false, # ビューファイル用のスペックを作成しない
-        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
-        routing_specs: false # routes.rb用のスペックファイル作成しない
+                       fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+                       view_specs: false, # ビューファイル用のスペックを作成しない
+                       helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+                       routing_specs: false # routes.rb用のスペックファイル作成しない
     end  # ここまで追記
   end
 end
