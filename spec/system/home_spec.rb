bbb85d76d2a5a29f90c4e6282588580359c900b0
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-09-17 01:28:52 +0900

    トップページの統合テストを追加

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :system do
+  before do # ここから追記
+    driven_by :selenium_chrome_headless
+  end # ここまで追記
+  
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'トップページの検証' do
+    it 'Home#top という文字列が表示される' do
+      visit '/'
+
+      expect(page).to have_content('Home#top')
+    end
+  end
+end
